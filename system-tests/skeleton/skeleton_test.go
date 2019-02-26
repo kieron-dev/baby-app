861dd75de2f18b7e996f9da887799f91406217f0
Kieron Browne <kbrowne@pivotal.io>
2019-02-26 21:49:25 +0000

    skeleton test
    the beginning...

---
+++ system-tests/skeleton/skeleton_test.go
@@ -0,0 +1,34 @@
+package skeleton_test
+
+import (
+	// . "github.com/kieron-pivotal/baby-app/system-tests/skeleton"
+
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/agouti"
+	. "github.com/sclevine/agouti/matchers"
+)
+
+var _ = Describe("Skeleton", func() {
+	var page *agouti.Page
+
+	BeforeEach(func() {
+		var err error
+		page, err = agoutiDriver.NewPage()
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		Expect(page.Destroy()).To(Succeed())
+	})
+
+	It("can see the home page", func() {
+		url := os.Getenv("APP_URL")
+		Expect(url).ToNot(BeEmpty())
+
+		Expect(page.Navigate(url)).To(Succeed())
+		Eventually(page.FindByName("q")).Should(BeFound())
+	})
+})
