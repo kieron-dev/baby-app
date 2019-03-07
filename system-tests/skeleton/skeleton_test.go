d8123b90e8b09e490f9d7f2094373a4673eba766
Kieron Browne <kbrowne@pivotal.io>
2019-03-07 21:15:23 +0000

    try to select the one element of the class

--- system-tests/skeleton/skeleton_test.go
+++ system-tests/skeleton/skeleton_test.go
@@ -29,6 +29,6 @@ var _ = Describe("Skeleton", func() {
 		Expect(url).ToNot(BeEmpty())
 
 		Expect(page.Navigate(url)).To(Succeed())
-		Eventually(page.FindByClass("App")).Should(MatchText("hello, world"))
+		Eventually(page.FirstByClass("App")).Should(MatchText("hello, world"))
 	})
 })
